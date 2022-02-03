5bc187f02546a9f32212932d6cc621c81e38bd9d
Rajendra Mudale <rajmudale@yahoo.co.in>
2022-02-03 22:38:05 +0530

    Create native_method_string_repeater.js

---
+++ native_method_string_repeater.js
@@ -0,0 +1,17 @@
+/*
+ * Define a repeatify function on the String Object. The function accepts an integer that specifies how many times the string has to be repeated
+ * The Function returns the string repeated the number of times specified 
+ * e.g console.log("Javascript".repeatify(3));
+ */
+ 
+ String.prototype.repeatify = String.prototype.repeatify || function(times) {
+  let str = "";
+  
+  for(let i=0; i<times; i++) {
+    str += this;
+  }
+  
+  return str; 
+ }
+ 
+ console.log("Javascript".repeatify(3))
